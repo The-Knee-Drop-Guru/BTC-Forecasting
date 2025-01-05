c865e3ef22e36a77be3f34b4b94ad04133ca4b06
kingodjerry <ws.yjkim@gmail.com>
2025-01-05 16:53:53 +0900

    login and signup

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .forms import CustomLoginForm
+
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboard, name='index'),
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='dashboard/login.html',
+        authentication_form=CustomLoginForm
+    ), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='dashboard:index'), name='logout'),
+    path('signup/', views.signup, name='signup'),
     path('forecast/', views.forecast, name='forecast'),
 ]
